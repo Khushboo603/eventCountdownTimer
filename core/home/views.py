d181c6fde9e8f3ab92b18f8c4e7f4e82c8c7c37b
Khushboo Kumari <khushboo.kumari@nusummit.com>
2026-02-13 18:02:03 +0530

    model for table and view for countdownTimer url done

--- core/home/views.py
+++ core/home/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+def countdown_timer(request):
+    event = Event.objects.first()
+    if event:
+        time_remaining = event.event_date - timezone.now()
+        hours = time_remaining.seconds
+        minutes = (time_remaining.seconds % 3600)
+        seconds = time_remaining.seconds % 60
+        data = {
+            'name': event.name
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        }
+    else:
+        data = {
+            'name': "No Event",
+            'hours': 0,
+            'minutes': 0,
+            'seconds': 0
+        }
+    return render(request, 'myapp.html', {'data': data})
